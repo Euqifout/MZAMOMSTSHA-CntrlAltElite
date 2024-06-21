80347e7e398503e20478ebc8a6630674aef15a3f
Euqifout <165257449+Euqifout@users.noreply.github.com>
2024-06-21 15:57:28 +0200

    .

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const initSlider = () => {
     const imageList = document.querySelector(".rightContent .service_box");
     const slideButtons = document.querySelectorAll(".rightContent .slide-Btn-mp");
-    
+    const slideScrollbar = document.querySelector(".service_box .main-slider-scrollbar");
+    const slideScroThumb= slideScrollbar.querySelector(".main-scrollbar-thumb");
+
     slideButtons.forEach(button => {
        button.addEventListener("click", () => {
             const direction = button.id === "prev-slide-mp" ? -1 : 1 ;
